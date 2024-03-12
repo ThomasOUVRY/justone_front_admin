20fffcbcecc1d14bbda7d8406e7b58126f1759bc
Thomas OUVRY <thomas.ouvry@KIABI.com>
2024-03-12 23:51:56 +0100

    add daisy ui, begin list of games

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,10 @@ export type ButtonProps = {
 }
 
 const Button = ({onClick, children}: ButtonProps) => {
-    return <button onClick={onClick}>{children}</button>
+    return <button onClick={onClick}
+        className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+        {children}
+    </button>
 }
 
 export default Button;
--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,31 +1,57 @@
 import {createLazyFileRoute, useNavigate} from '@tanstack/react-router'
 import Button from "../components/Button.tsx";
+import {useEffect, useState} from "react";
 
 export const Route = createLazyFileRoute('/')({
     component: Index,
 })
 
+type Game = {
+    id: number;
+    type: string;
+    nbPlayers: number;
+    status: string;
+}
+
 function Index() {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const [games, setGames] = useState<Game[]>([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/games")
+            .then(response => response.json())
+            .then((data: Game[]) => setGames(data));
+    }, []);
 
     return (
         <div className="p-2">
             <Button onClick={() => navigate({to: '/new-game'})}>Go to about</Button>
+            <h1 className="text-2xl">Hello World</h1>
 
-            <table>
-                <th>titi</th>
-                <th>toto</th>
-                <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>2</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>4</td>
-                </tr>
-                </tbody>
-            </table>
+            <div className="overflow-x-auto">
+                <div className="overflow-x-auto">
+                    <table className="table">
+                        <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Number of players</th>
+                            <th>Status</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {games.map(game => (
+                            <tr key={game.id}>
+                                <td>{game.id}</td>
+                                <td>{game.type}</td>
+                                <td>{game.nbPlayers}</td>
+                                <td>{game.status}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     )
 }
